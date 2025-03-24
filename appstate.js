751b9bafda8af73be8c0a10a9443a88aeb5e1c49
NgocUyenEris <148871478+NgocUyenEris@users.noreply.github.com>
2025-03-24 13:39:42 +0700

    Update appstate.js

--- appstate.js
+++ appstate.js
@@ -1,1 +1,110 @@
-//thích thì add appstate vô để cho nó chạy command
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261563835167426%22%3A%22h88hs19q%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.668Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "dpr",
+        "value": "1.4375",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "datr",
+        "value": "eOLgZ25Q15KtvXOxiaWs7C-I",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "sb",
+        "value": "eOLgZ6FGdvYPd4l30FOS1Nhr",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "locale",
+        "value": "vi_VN",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61563835167426",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "xs",
+        "value": "10%3Ag_pebYVn_LArRA%3A2%3A1742796711%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "fr",
+        "value": "0kgaNjysbGsje5XHe.AWXDzECwakHAlhpJ7GM0HcahTrx0TFrd8Hl87A.Bn4OJ4..AAA.0.0.Bn4Pkn.AWWoC6PfKB0",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "wd",
+        "value": "501x992",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    },
+    {
+        "key": "presence",
+        "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1742797103500%2C%22v%22%3A1%7D",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-03-24T06:18:34.669Z",
+        "lastAccessed": "2025-03-24T06:18:34.669Z"
+    }
+]//thích thì add appstate vô để cho nó chạy command
